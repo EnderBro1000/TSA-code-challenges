06d03e3b7a0c0aeeb3068334881f86d0dd539d6e
Ender <rockers4403@gmail.com>
2021-12-02 21:59:37 -0700

    Refines Monogram Challenge

--- challenges/monogram.py
+++ challenges/monogram.py
@@ -1,18 +1,6 @@
 import sys
 
-
-# file = sys.stdin.read()
-file = """
-2
-2
-Franklin Delano Roosevelt
-h ross perot
-4
-Dwight David Eisenhower
-Warren Gamaliel Harding
-Harry S Truman
-John Fitzgerald Kennedy
-"""
+file = sys.stdin.read()
 
 file = ''.join([i for i in file if not i.isdigit()]) # removes digits
 file = file.split("\n") # splits lines into list of strings
@@ -20,7 +8,7 @@ file = [i for i in file if i != ""] # removes blank lines
 
 monogrammed = []
 for line in file:
-    line = line.split(" ")
-    monogrammed.append(f"{line[0][0]}{line[2][0]}{line[1][0]}".upper())
+    line = line.split(" ") # split full name into each word
+    monogrammed.append(f"{line[0][0]}{line[2][0]}{line[1][0]}".upper()) # get uppercase monogram
 
-print(''.join([i + '\n' for i in monogrammed[:-1]]) + monogrammed[-1])
+[print(i) for i in monogrammed]
